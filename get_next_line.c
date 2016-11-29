ad35228e942f894555ced0da751fcee9af461afe
Nicolas Bouchinet <nicolas.bouchinet@student.42.fr>
2016-11-29 16:43:55 +0100

    adding

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nbouchin <nbouchin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/24 13:21:55 by nbouchin          #+#    #+#             */
-/*   Updated: 2016/11/29 15:11:54 by nbouchin         ###   ########.fr       */
+/*   Updated: 2016/11/29 15:30:19 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	char		buff[BUFF_SIZE + 1];
+	char			buff[BUFF_SIZE + 1];
+	char static		*static_buff;
 
-	*line = ft_strnew(1);
+	*line = ft_strnew(0);
 	while (!ft_strchr(buff, '\n'))
 	{
 		read(fd, &buff, BUFF_SIZE);
@@ -27,10 +28,10 @@ int		get_next_line(const int fd, char **line)
 
 int main (int argc, char **argv)
 {
-	char *test;
+	   char *test;
 
-	get_next_line(0, &test);
-	ft_putstr("========= line ==========\n");
-	ft_putstr(test);
-	ft_putstr("====== End of Line ======\n");
+	   get_next_line(0, &test);
+	   ft_putstr("========= line ==========\n");
+	   ft_putstr(test);
+	   ft_putstr("====== End of Line ======\n");
 }
